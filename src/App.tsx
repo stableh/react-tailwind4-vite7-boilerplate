916d82032b123c9ba4be340e16f66ae72a8514b2
stableh <ajhrc08070@gmail.com>
2025-12-22 10:56:21 +0900

    🏗️ Fix: remove todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
-import TodoApp from './components/TodoApp'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout'
+import Home from './pages/Home'
+import About from './pages/About'
 
 function App() {
-  return <TodoApp />
+  return (
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
+  )
 }
 
 export default App
